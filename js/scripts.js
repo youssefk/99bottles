46939a216ca9b7ad90a2af28c5313eada6420826
Youssef <youssefkhalifa@live.com>
2015-10-14 20:35:57 -0700

    FIX THE DANG CODE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-  
+    for (var bottles = 99; bottles >= 1; bottles -= 1) {
+      $("#lyrics").append("<p>" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer. Take one down, pass it around " + (bottles - 1) + " bottles of beer on the wall." + "</p>");
+    }
 });
